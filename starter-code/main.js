86040308dcfd427430e3741d757a7b0511c40f60
JorgeJoseAbad <jorgejoseabad@gmail.com>
2021-03-13 18:37:29 +0100

    Case 15

--- starter-code/main.js
+++ starter-code/main.js
@@ -230,6 +230,21 @@ MongoClient.connect(url, (error, db) => {
           });
           break;
 
+          case "15":
+          db.collection('companies').find({tag_list:{$in:["social-networking"]}},{name:1,_id:0}).toArray((err,docs)=>{
+            if (err) {
+              console.log(err);
+              rl.question(`\nType enter to continue: `,(answer)=>{mainMenu();});
+            } else {
+              console.log(docs);
+              docs.forEach((item, i) => {
+                console.log(item.name);
+              });
+              rl.question(`\nType enter to continue: `,(answer)=>{mainMenu();});
+            }
+          });
+          break;
+
           case "0":
             console.log(`👋👋👋👋 😞 \n`);
             db.close((error) => { process.exit(0); });
